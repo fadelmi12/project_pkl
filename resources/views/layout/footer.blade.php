743d025a3543c407d7e18588ede2ae81adf70d89
riannomp <riannomanunggal@gmail.com>
2021-06-12 09:56:05 +0700

    nonono

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="footer container-fluid pl-30 pr-30">
     <div class="row">
         <div class="col-sm-12">
-            <p>2017 &copy; Doodle. Pampered by Hencework</p>
+            <p>2021 &copy; CV. Nakula Sadewa</p>
         </div>
     </div>
 </footer>
