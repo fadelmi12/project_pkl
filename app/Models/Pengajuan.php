80a6520fbc10a40113d8e14564f777f894b638bd
lailaa7 <ti.lailafdh7@gmail.com>
2021-06-26 10:34:56 +0700

    pegajuan

---
+++ app/Models/Pengajuan.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Pengajuan extends Model
+{
+    use HasFactory;
+    protected $table = "pengajuan";
+    protected $primaryKey = "id_pengajuan";
+    protected $fillable = ['id_pengajuan', 'noPO','namaBarang','jenisBarang','jmlBarang','status','keterangan', 'pic_teknisi'
+                            , 'pic_marketing','pic_warehouse','created_at'];
+                            
+
+    public function pengajuan()
+    {
+        return $this->hasMany(pengajuan::class);
+    }
+
+    public function editBaru($id_pengajuan, $data_baru)
+    {
+        DB::table('pengajuan')
+            ->where('id_pengajuan', $id_pengajuan)
+            ->update($data_baru);
+    }
+}
