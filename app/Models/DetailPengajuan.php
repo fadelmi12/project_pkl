2bbb38561503fc1088053ab352a2bca6502b26bf
Hadafi337 <85341971+Hadafi337@users.noreply.github.com>
2021-08-05 13:51:42 +0700

    Update DetailPengajuan.php

--- app/Models/DetailPengajuan.php
+++ app/Models/DetailPengajuan.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class DetailPengajuan extends Model
 {
     use HasFactory;
+    protected $table = "detail_pengajuan";
+    protected $primaryKey = "id_detailPengajuan";
+    protected $fillable = ['id_pengajuan','kode', 'noPO','namaBarang','jenisBarang','jmlBarang','keterangan','created_at'];
+
+
+    public function detail_pengajuan()
+    {
+        return $this->hasMany(detail_pengajuan::class);
+    }
 }
