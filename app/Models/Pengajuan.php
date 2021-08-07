a23f32a5cfb04ba2ccbf33e1d90e49d668d6e9cb
Hadafi337 <85341971+Hadafi337@users.noreply.github.com>
2021-08-07 13:28:45 +0700

    Update Pengajuan.php

--- app/Models/Pengajuan.php
+++ app/Models/Pengajuan.php
@@ -11,7 +11,7 @@ class Pengajuan extends Model
     use HasFactory;
     protected $table = "pengajuan";
     protected $primaryKey = "id_pengajuan";
-    protected $fillable = ['id_pengajuan','kode', 'noPO','judul','namaBarang', 'jumlah', 'keterangan', 'status', 'pic_teknisi'
+    protected $fillable = ['id_pengajuan','kode', 'noPO','judul', 'status','jenisBarang', 'pic_teknisi'
                             , 'pic_marketing','pic_warehouse','created_at'];
                             
 
