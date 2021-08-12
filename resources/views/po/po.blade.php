7d242a77e463c384dd7a99d6e51a66c08cc913f2
lailaa7 <ti.lailafdh7@gmail.com>
2021-08-12 12:45:53 +0700

    purchase

--- resources/views/po/po.blade.php
+++ resources/views/po/po.blade.php
@@ -2,6 +2,10 @@
 @section('title', 'Data Purchase Order')
 @section('content')
 
+
+<!-------------------------------------------------------------- Marketing ------------------------------------------------------->
+@if (auth()->user()->divisi == "marketing")
+
 <!-- Main Content -->
 <div class="page-wrapper">
     <div class="container-fluid">
@@ -25,10 +29,7 @@
                 <div class="panel panel-default card-view">
                     <div class="panel-heading">
                         <div class="pull-left">
-                            <!-- <h6 class="panel-title txt-dark">DataTable</h6> -->
-                            <!-- <button data-toggle="modal" data-target="#myModal" class="btn btn-success col-mr-2" >Tambah Barang</button> -->
-                            <a href="addpo"> <button class="btn btn-success btn-icon-anim" data-toggle="modal" data-target="#addpo"> Tambah Data</button> </a>
-                            <!-- <button class="btn btn-primary btn-sm btn-icon mb-3"><i class="fa fa-plus fa-sm"></i> Tambah Data</button> -->
+                            <a href="addpo"> <button class="btn btn-success btn-icon-anim"> Tambah Data</button> </a>
                         </div>
 
                         <div class="clearfix"></div>
@@ -104,7 +105,130 @@
         <!-- /Row -->
     </div>
 </div>
-<!-- /Main Content -->
+@endif
+
+@if (auth()->user()->divisi == "warehouse"||auth()->user()->divisi == "admin")
+
+<!-- Main Content -->
+<div class="page-wrapper">
+    <div class="container-fluid">
+        <!-- Title -->
+        <div class="row heading-bg">
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                <h5 class="txt-dark">Data Purchasing</h5>
+            </div>
+            <!-- Breadcrumb -->
+            <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                <ol class="breadcrumb">
+                    <!-- <li><a href="inventory"></a></li> -->
+                    <li class="active"><span>Data Purchasing</span></li>
+                </ol>
+            </div>
+            <!-- /Breadcrumb -->
+        </div>
+        <!-- Row -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="panel panel-default card-view">
+                    <div class="panel-heading">
+                        <div class="pull-left">
+                            <a href="addpo"> <button class="btn btn-success btn-icon-anim"> Tambah Data</button> </a>
+                        </div>
+
+                        <div class="clearfix"></div>
+                        <div id="myTable1_wrapper" class="dataTables_wrapper">
+                            <div class="dataTables_length" id="myTable1_length"><label>Show <select name="myTable1_length" aria-controls="myTable1" class="">
+                                        <option value="10">10</option>
+                                        <option value="25">25</option>
+                                        <option value="50">50</option>
+                                        <option value="100">100</option>
+                                    </select> entries</label></div>
+                            <div id="myTable1_filter" class="dataTables_filter"><label>Search:<input type="search" class="" placeholder="" aria-controls="myTable1"></label></div>
+                            <table id="myTable1" class="table table-hover display dataTable dtr-inline" role="grid" aria-describedby="myTable1_info" style="width: 1253px;">
+                        </div>
+                    </div>
+                    <div class="panel-wrapper collapse in">
+                        <div class="panel-body">
+                            <div class="table-wrap">
+                                <div class="">
+                                    <table id="myTable1" class="table table-bordered display  pb-30">
+                                        <thead>
+                                            <tr>
+                                                <th>no</th>
+                                                <th>No PO</th>
+                                                <th>Nama Barang</th>
+                                                <th>Jumlah</th>
+                                                <th>Created at</th>
+                                                <th>Keterangan</th>
+                                                <th>Status</th>
+                                                <th colspan="3">Aksi</th>
+                                            </tr>
+                                        </thead>
+                                        <!-- <tfoot>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Position</th>
+                                                <th>Office</th>
+                                                <th>Age</th>
+                                                <th>Start date</th>
+                                                <th>Salary</th>
+                                            </tr>
+                                        </tfoot> -->
+                                        <tbody>
+                                            <?php $no = 1; ?>
+                                            @foreach ($data_po as $data_po)
+                                                <tr>
+                                                @if (auth()->user()->divisi == "admin")
+                                                    @if($data_baru->status >=1 )
+                                                    <td>{{ $no++ }}</td>
+                                                    <td>{{ $data_po->no_PO}}</td>
+                                                    <td>{{ $data_po->namaBarang}}</td>
+                                                    <td>{{ $data_po->jumlah}}</td>
+                                                    <td>{{ $data_po->created_at}}</td>
+                                                    <td>{{ $data_po->keterangan}}</td>
+                                                    <td>{{ $data_po->status}}</td>
+                                                    <td>
+                                                        <button class="btn btn-success btn-icon-anim btn-square" data-toggle="modal" data-target="#detail"><i class="fa fa-edit"></i></button>
+                                                        <button class="btn btn-primary btn-icon-anim btn-square" data-toggle="modal" data-target="#editpinjam"><i class="fa fa-pencil"></i></button>
+                                                        <button class="btn btn-danger btn-icon-anim btn-square"><i class="fa fa-trash" data-toggle="modal" data-target="#hapuspinjam"></i></button>
+                                                        @include('peminjaman.edit')
+                                                        <!-- <div class="btn btn-round btn-danger btn-sm btn-icon"><i class="fa fa-trash"></i></div> -->
+                                                    </td>
+                                                     @endif
+                                               
+                                                @elseif (auth()->user()->divisi == "warehouse")
+                                                    <td>{{ $no++ }}</td>
+                                                    <td>{{ $data_po->no_PO}}</td>
+                                                    <td>{{ $data_po->namaBarang}}</td>
+                                                    <td>{{ $data_po->jumlah}}</td>
+                                                    <td>{{ $data_po->created_at}}</td>
+                                                    <td>{{ $data_po->keterangan}}</td>
+                                                    <td>{{ $data_po->status}}</td>
+                                                    <td>
+                                                        <button class="btn btn-success btn-icon-anim btn-square" data-toggle="modal" data-target="#detail"><i class="fa fa-edit"></i></button>
+                                                        <button class="btn btn-primary btn-icon-anim btn-square" data-toggle="modal" data-target="#editpinjam"><i class="fa fa-pencil"></i></button>
+                                                        <button class="btn btn-danger btn-icon-anim btn-square"><i class="fa fa-trash" data-toggle="modal" data-target="#hapuspinjam"></i></button>
+                                                        @include('peminjaman.edit')
+                                                        <!-- <div class="btn btn-round btn-danger btn-sm btn-icon"><i class="fa fa-trash"></i></div> -->
+                                                    </td>
+                                                @endif
+                                                @include('peminjaman.hapus')
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                    @include('peminjaman.addpinjam')
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /Row -->
+    </div>
 </div>
-<!-- /#wrapper -->
+
+@endif
+
 @endsection
