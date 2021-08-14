60ced8e1958ed61ce36ef00eec906ed561c6e39e
Hadafi337 <85341971+Hadafi337@users.noreply.github.com>
2021-08-14 12:46:19 +0700

    Update log.blade.php

--- resources/views/administrator/log.blade.php
+++ resources/views/administrator/log.blade.php
@@ -6,20 +6,20 @@
 <div class="page-wrapper">
     <div class="container-fluid">
         <!-- Title -->
-        <div class="row heading-bg">
-            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                <h5 class="txt-dark">Log Aktivitas</h5>
-            </div>
+        <!-- <div class="row heading-bg">
+            <div class="col-lg-9 col-md-4 col-sm-4 col-xs-5"> -->
+                <!-- <h5 class="txt-dark">Log Aktivitas</h5> -->
+            <!-- </div> -->
             <!-- Breadcrumb -->
-            <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+            <!-- <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                 <ol class="breadcrumb">
                     <li class="active"><span>Log Aktivitas</span></li>
                 </ol>
-            </div>
+            </div> -->
             <!-- /Breadcrumb -->
-        </div>
+        <!-- </div> -->
         <!-- Row -->
-        <div class="col-lg-12 col-md-12 mt-10">
+        <div class="col-lg-12 col-md-12 mt-40">
 			<div class="panel panel-default card-view">
 				<div class="panel-heading">
 					<div class="pull-left">
@@ -38,22 +38,27 @@
                             <!-- BARANG -->
 							<div class="tab-content" id="myTabContent_7">
 								<div  id="admin" class="tab-pane fade active in" role="tabpanel">
-								<table id="data_table1" class="table table-bordered display  pb-30">
+								<table id="datable_1" class="table table-bordered display  pb-30">
                                 <thead>
                                             <tr>
+                                                <th>no</th>
                                                 <th>Nama </th>
                                                 <th>Email</th>
                                                 <th>Divisi</th>
+                                                <th>IP Address</th>
                                                 <th>Deskripsi</th>
                                                 <th>Waktu</th>
                                             </tr>
                                         </thead>
                                         <tbody>
+                                            <?php $no = 1; ?>
                                             @foreach ($log as $log)
                                             <tr>    
+                                                <td>{{ $no++ }}</td>
                                                 <td>{{ $log->name}}</td>
                                                 <td>{{ $log->email}}</td>
                                                 <td>{{ $log->divisi}}</td>
+                                                <td>{{ $log->ip}}</td>
                                                 <td>{{ $log->deskripsi}}</td>
                                                 <td>{{ $log->created_at}}</td>
                                             </tr>
@@ -62,27 +67,33 @@
                                     </table>
                                 </div>
 								
-                                <!-- BARANG RETUR -->
+                                <!-- aksi -->
                                 <div  id="admin2" class="tab-pane fade" role="tabpanel">
-                                    <table id="data_table1" class="table table-bordered display  pb-30">
+                                    <table id="datable_1" class="table table-bordered display  pb-30">
                                         <thead>
                                             <tr>
+                                                <th>no</th>
                                                 <th>Nama </th>
                                                 <th>Email</th>
                                                 <th>Divisi</th>
+                                                <th>IP Address</th>
                                                 <th>Deskripsi</th>
                                                 <th>Waktu</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            
-                                            <tr>    
-                                                <td></td>
-                                                <td></td>
-                                                <td></td>
-                                                <td></td>
-                                                <td></td>
+                                            <?php $no = 1; ?>
+                                            @foreach ($log2 as $log)
+                                            <tr>
+                                                <td>{{ $no++ }}</td>    
+                                                <td>{{ $log->name}}</td>
+                                                <td>{{ $log->email}}</td>
+                                                <td>{{ $log->divisi}}</td>
+                                                <td>{{ $log->ip}}</td>
+                                                <td>{{ $log->deskripsi}}</td>
+                                                <td>{{ $log->created_at}}</td>
                                             </tr>
+                                            @endforeach
                                         </tbody>
                                     </table>
 								</div>
