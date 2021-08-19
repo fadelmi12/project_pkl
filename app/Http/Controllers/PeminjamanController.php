663888b5841662b2a7b3cb8cd685198c45948ec8
Hadafi337 <85341971+Hadafi337@users.noreply.github.com>
2021-08-19 16:47:06 +0700

    Update PeminjamanController.php

--- app/Http/Controllers/PeminjamanController.php
+++ app/Http/Controllers/PeminjamanController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Log;
 use App\Models\Peminjaman;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PeminjamanController extends Controller
 {
@@ -22,31 +24,39 @@ class PeminjamanController extends Controller
     public function addpinjam2(Request $request)
     {
         $rules = [
-            'nama' => 'required',
             'nama_barang' => 'required',
             'jumlah' => 'required',
             'keterangan' => 'required',
-            'tgl_pinjam' => 'required',
+            'tgl_kembali' => 'required',
         ];
 
         $messages = [
-            'nama.required' => '*Nama tidak boleh kosong',
             'nama_barang.required' => '*Nama barang tidak boleh kosong',
             'jumlah.required' => '*Jumlah tidak boleh kosong',
             'keterangan.required' => '*Keterangan tidak boleh kosong',
-            'tgl_pinjam.required' => '*Tgl pinjam tidak boleh kosong',
+            'tgl_kembali.required' => '*Tanggal kembali tidak boleh kosong',
         ];
         $this->validate($request, $rules, $messages);
-
+        $user = Auth::user();
         Peminjaman::create([
-            'nama'          => $request->nama,
+            'nama'          => $user->name,
             'barang'   => $request->nama_barang,
             'jumlah'        => $request->jumlah,
             'keterangan'    => $request->keterangan,
-            'tglPinjam'     => $request->tgl_pinjam,
-            // 'tglKembali'    => $request->tgl_kembali,
-            'status'        => 'pinjam'
+            'tglKembali'    => $request->tgl_kembali,
         ]);
+        $user = Auth::user();
+        Log::create(
+            [
+            'name' => $user->name,
+            'email' => $user->email,
+            'divisi' => $user->divisi,
+            'deskripsi' => 'Create Peminjaman',
+            'status' => '2',
+            'ip'=> $request->ip()
+
+            ]
+        );
         return redirect('peminjaman');
     }
 
@@ -58,23 +68,51 @@ class PeminjamanController extends Controller
 
     public function updatePinjam(Request $request)
     {
-
-        if ($request->edit_tgl_kembali) {
-            $status = 'dikembalikan';
-        } else {
-            $status = 'pinjam';
-        }
         Peminjaman::where('id_peminjaman', $request->edit_id_pinjam)
             ->update([
-                'nama'          => $request->edit_nama,
                 'barang'   => $request->edit_nama_barang,
                 'jumlah'        => $request->edit_jumlah,
                 'keterangan'    => $request->edit_keterangan,
-                'tglPinjam'     => $request->edit_tgl_pinjam,
                 'tglKembali'    => $request->edit_tgl_kembali,
-                'status'        => $status
             ]);
+            $user = Auth::user();
+            Log::create(
+                [
+                'name' => $user->name,
+                'email' => $user->email,
+                'divisi' => $user->divisi,
+                'deskripsi' => 'Update Peminjaman',
+                'status' => '2',
+                'ip'=> $request->ip()
+    
+                ]
+            );
         return redirect('peminjaman');
         // return redirect()->back();
+    }
+
+        public function deletepinjam($id_peminjaman, Request $request)
+    {
+        // dd($id_jenis);
+        // dd($id_master);
+        // $data_kategori = Master::find($request->id_master);
+        $pinjam = Peminjaman::where('id_peminjaman', $id_peminjaman)->first();
+        // // dd($barang);
+        $pinjam->delete();
+
+        $user = Auth::user();
+        Log::create(
+            [
+            'name' => $user->name,
+            'email' => $user->email,
+            'divisi' => $user->divisi,
+            'deskripsi' => 'Delete Pengajuan Barang Retur',
+            'status' => '2',
+            'ip'=> $request->ip()
+
+            ]
+        );
+        // //mengirim data_ktg ke view
+        return redirect('peminjaman')->with('success', "Data telah terhapus");
     }
 }
