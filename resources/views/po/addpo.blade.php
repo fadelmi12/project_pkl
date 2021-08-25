1d6e27987b112efa07722f90f156c7a0cae60a63
Hadafi337 <85341971+Hadafi337@users.noreply.github.com>
2021-08-25 18:32:50 +0700

    Update addpo.blade.php

--- resources/views/po/addpo.blade.php
+++ resources/views/po/addpo.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.master')
-@section('title', 'Data Pengajuan')
+@section('title', 'Tambah Purchase Order')
 @section('content')
 
 <!-- Main Content -->
@@ -39,44 +39,128 @@
                                         <input type="text" id="noPO" name="noPO" class="form-control" readonly>
                                     </div> -->
                                     @foreach ((array)$noPO as $noPO)
-                                    <div class="form-group">
-                                        <label class="control-label mb-10 text-left" for="example-email">No Purchase Order</label>
-                                        <input type="text" id="noPO" name="noPO" value="{{ $noPO }}" class="form-control" placeholder="" readonly>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label class="control-label mb-10 text-left" for="example-email">No Purchase Order</label>
+                                                <input type="hidden" id="no_PO" name="no_PO" value="{{ $noPO }}" class="form-control" placeholder="" readonly>
+                                                <input type="text" id="noPO" name="noPO" value="{{ $noPO }}" class="form-control" placeholder="" readonly>
+                                            </div>
+                                        </div>
+                                        @endforeach
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label class="control-label mb-10 text-left" for="example-email">Instansi<span class="help"> </span></label>
+                                                <input type="text" id="instansi" name="instansi" class="form-control" placeholder="">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+											<div class="form-group">
+												<label class="control-label mb-10">Tanggal Pemasangan</label>
+												<input type="date" name="tgl_transaksi" id="tgl_transaksi" class="form-control" >
+											</div>
+										</div>
+                                    </div>
+                                    <h5 class="active">Data Barang</h5>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label class="control-label mb-10 text-left">Nama barang</label>
+                                                <input type="text" class="form-control" name="nama_barang" id="nama_barang">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label class="control-label mb-10">Jumlah</label>
+                                                <input type="number" id="jumlah" name="jumlah" class="form-control">
+                                                <!-- <span class="help-block"> This is inline help </span>  -->
+                                            </div>
+                                        </div>
                                     </div>
                                     <div class="form-group">
-                                        <label class="control-label mb-10 text-left" for="example-email">Nama Barang</label>
-                                        <input type="text" id="namaBarang" name="namaBarang" class="form-control" placeholder="">
-                                        @if ($errors->has('namaBarang'))
-                                        <div class="tulisan">{{$errors->first('namaBarang')}}</div>
-                                        @endif
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label mb-10 text-left" for="example-email">Jumlah Barang</label>
-                                        <input type="number" id="jumlah" name="jumlah" class="form-control" placeholder="">
-                                        @if ($errors->has('jumlah'))
-                                        <div class="tulisan">{{$errors->first('jumlah')}}</div>
-                                        @endif
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label mb-10 text-left" for="example-email">Keterangan </label>
-                                        <textarea type="text" id="keterangan" name="keterangan" class="form-control"></textarea>
+                                        <label class="control-label mb-10 text-left" for="example-email">Keterangan<span class="help"> </span></label>
+                                        <input type="text" id="keterangan" name="keterangan" class="form-control" placeholder="">
                                     </div>
                                     <div class="form-group" style="text-align:right;">
-                                        <button class="btn btn-success">Simpan</button>
-                                        <!-- <button class="btn btn-danger  " name="reset" type="reset">Batal
-                                        </button> -->
+                                        <button type="button" onclick="ambildata()" class="btn btn-primary ">Tambah Data</button>
                                     </div>
-                                    @endforeach
-                                </form>
 
+                                    <div class="col-sm-14">
+                                        <div class="panel panel-default card-view">
+                                            <div class="panel-heading">
+                                                <div class="pull-left">
+                                                    <h6 class="panel-title txt-dark">Barang Masuk</h6>
+                                                </div>
+                                                <div class="clearfix"></div>
+                                            </div>
+                                            <div class="panel-wrapper collapse in">
+                                                <div class="panel-body">
+                                                        <div class="">
+                                                            <div class="col">
+                                                                <table class="table table-bordered align-items-center">
+                                                                    <thead class="thead-light">
+                                                                        <tr>
+                                                                            <th>No PO</th>
+                                                                            <th>Nama barang</th>
+                                                                            <th>Jumlah</th>
+                                                                            <th>Keterangan</th>
+                                                                            <th>Remove</th>
+                                                                        </tr>
+                                                                    </thead>
+                                                                    <tbody id="TabelDinamis">
+                                                                    </tbody>
+                                                                </table>
+                                                                @error('TabelDinamis')
+                                                                <div class="tulisan">{{$message}}</div>
+                                                                @enderror
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12" style="text-align:right;">
+                                                <button type="submit" class="btn btn-primary ">Simpan</button>
+                                            </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="">
+
+            <!-- Basic Table -->
+
+        </div>
         <!-- /Row -->
         <!-- /Main Content -->
     </div>
     <!-- /#wrapper -->
+    @endsection
+    @section('scripts')
+    <script type="text/javascript">
+        function ambildata() {
+            var noPO = document.getElementById('noPO').value;
+            var nama_barang = document.getElementById('nama_barang').value;
+            var jumlah = document.getElementById('jumlah').value;
+            var keterangan = document.getElementById('keterangan').value;
+            addrow(noPO, nama_barang, jumlah, keterangan);
+        }
+        var i = 0;
+
+        function addrow(noPO, nama_barang, jumlah, keterangan) {
+            i++;
+            $('#TabelDinamis').append('<tr id="row' + i + '"><td><input type="text" style="outline:none;border:0;" readonly name="noPO[]" id="noPO" value="' + noPO +
+                                                            '"><td><input type="text" style="outline:none;border:0;" readonly name="nama_barang[]" id="nama_barang" value="' + nama_barang + 
+                                                            '"></td><td><input type="text" style="outline:none;border:0;" name="jumlah[]" id="jumlah" value="' + jumlah + 
+                                                            '"></td><td><input type="text" style="outline:none;border:0;" name="keterangan[]" id="keterangan" value="' + keterangan + 
+                                                            '"></td><td><button type="button" id="' + i + '" class="btn btn-danger btn-small remove_row">&times;</button></td></tr>');
+        };
+        $(document).on('click', '.remove_row', function() {
+            var row_id = $(this).attr("id");
+            $('#row' + row_id + '').remove();
+        });
+    </script>
     @endsection
