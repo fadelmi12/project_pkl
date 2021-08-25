9d973e0280c8785b47c78e19955d5f25a54c6a0e
Hadafi337 <85341971+Hadafi337@users.noreply.github.com>
2021-08-25 18:46:57 +0700

    Update 2021_06_12_021314_detail_po_table.php

--- database/migrations/2021_06_12_021314_detail_po_table.php
+++ database/migrations/2021_06_12_021314_detail_po_table.php
@@ -16,10 +16,10 @@ class DetailPoTable extends Migration
         Schema::create('detail_po', function (Blueprint $table) {
             $table->increments('id_po');
             $table->string('no_PO',50);
-            $table->string('kode_barang',50);
-            $table->integer('kategori');
-            $table->integer('jml_barang');
-            $table->integer('status');
+            $table->string('nama_barang',50);
+            $table->integer('jumlah');
+            $table->string('keterangan')->nullable();
+            $table->integer('status')->nullable();
             $table->timestamps();
         });
     }
