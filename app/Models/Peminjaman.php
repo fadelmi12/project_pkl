3dbf6f7ed9918dfc5bd3cb932fef9eb2064898ec
Hadafi337 <85341971+Hadafi337@users.noreply.github.com>
2021-08-26 10:41:24 +0700

    Update Peminjaman.php

--- app/Models/Peminjaman.php
+++ app/Models/Peminjaman.php
@@ -12,5 +12,5 @@ class Peminjaman extends Model
     protected $table = "peminjaman";
     protected $primaryKey = "id_peminjaman";
 
-    protected $fillable = ['id_peminjaman', 'nama', 'barang', 'jumlah', 'keterangan', 'tglKembali', 'status'];
+    protected $fillable = ['id_peminjaman','no_peminjaman', 'kebutuhan', 'pic_teknisi', 'pic_warehouse', 'tglKembali', 'status'];
 }
